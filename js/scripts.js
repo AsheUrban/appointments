26509bab07d0323aae0e8d9da16e167745a88387
Ashe Urban <ashe@goldentongue.com>
2022-06-02 20:14:32 -0700

    WIP still trying to get #appointmentConfirm.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
-// Business Logic
-function input(name) {
-  return name;
-}
-function input(date) {
-  return date;
-}
-function input(time) {
-  return time;
-}
+// // Business Logic
+// function input(name) {
+//   return name;
+// }
+// function input(date) {
+//   return date;
+// }
+// function input(time) {
+//   return time;
+// }
 
 // UI Logic
 
 $(document).ready(function() {
   $("form#appointments").submit(function(event) {
-    event.preventDefault();
+  
       const nameInput= $("input#name").val();
       const dateInput= $("input#date").val();
       const timeInput= $("input#time").val();
@@ -23,6 +23,7 @@ $(document).ready(function() {
       $(".time").select(timeInput);
 
       $(#appointmentConfirm).show();
-   
+      
+    event.preventDefault();
   });
-}):
+});
